0888b4798a2e17807f8fbb202a86b2d858c199ad
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-03-26 23:58:50 +0530

    testing splitter part

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from backend.app.ingestion.loader import load_document
+from backend.app.ingestion.splitter import split_documents
 
-docs = load_document("file.txt")
-print(len(docs))
-print(docs[0].page_content[:200])
+docs = load_document("D:\\Code\\02_Project\Doc-Chat-AI\\backend\\app\\ingestion\\dsa-viva.pdf")
+chunks = split_documents(docs)
+
+print("Chunks : ", len(chunks))
+print(chunks[0].page_content[:200])
+# print(len(docs))
+# print(docs[0].page_content[:200])
