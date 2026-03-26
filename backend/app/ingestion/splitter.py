daf54012f50062f08b3e7da7e120587d670a81d6
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-03-26 23:53:58 +0530

    some minor corrections

--- backend/app/ingestion/splitter.py
+++ backend/app/ingestion/splitter.py
@@ -1,4 +1,4 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def split_documents(documents):
     splitter = RecursiveCharacterTextSplitter ( chunk_size = 500, chunk_overlap = 100)
