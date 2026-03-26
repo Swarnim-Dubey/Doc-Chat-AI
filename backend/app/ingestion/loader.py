f3eab57b3a1e22000b1ea901bad0d89d6fd9f766
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-03-26 15:48:31 +0530

    trying to handle pdf and text files

--- backend/app/ingestion/loader.py
+++ backend/app/ingestion/loader.py
@@ -1,6 +1,14 @@
-import pdfplumber
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
 
-with pdfplumber.open("dsa-viva.pdf") as pdf:
-    for i, page in enumerate(pdf.pages):
-        print(f"\n---Page{i}---")
-        print(page.extract_text())
+def load_document(file_path : str):
+    if file_path.endswith(".pdf"):
+        loader = PyPDFLoader(file_path)
+
+    elif file_path.endswith(".txt"):
+        loader = TextLoader(file_path)
+
+    else:
+        raise ValueError("Unsupported filetype")
+    
+    documents = loader.load()
+    return documents
