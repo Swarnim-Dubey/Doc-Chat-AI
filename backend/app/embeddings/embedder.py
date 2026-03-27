f52a8dbf86593dc5ce7376b7ade9fa33a2aa0929
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-03-27 23:30:48 +0530

    added google gemini as an embedder

--- backend/app/embeddings/embedder.py
+++ backend/app/embeddings/embedder.py
@@ -1,11 +1,17 @@
-from langchain_openai import OpenAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from dotenv import load_dotenv
+import os
 
+#loading env variables here
 load_dotenv()
 
-embeddings = OpenAIEmbeddings(model = "text-embedding-3-small")
+# initializing gemini embeddings
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="models/embedding-001",
+    google_api_key = os.getenv("AIzaSyBRcqxrn4j0HGwJNUb8ToYpq4YrQcDnUF8") # google gemini api key
+)
 
-def embed_text(text:str):
+def embed_text(text : str):
     return embeddings.embed_query(text)
 
 if __name__ == "__main__":
@@ -14,3 +20,5 @@ if __name__ == "__main__":
     
     print("Vector length:", len(vector))
     print(vector[:5])
+
+print("API KEY:", os.getenv("GOOGLE_API_KEY"))
