8d6d246507356771451ca540265f94602616e211
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-03-27 18:42:54 +0530

    testing embedding logic

---
+++ backend/app/embeddings/embedder.py
@@ -0,0 +1,7 @@
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+def get_embedding_model():
+    model = HuggingFaceEmbeddings(
+        model_name = "all-MiniLM-L6-v2"
+    )
+    return model
