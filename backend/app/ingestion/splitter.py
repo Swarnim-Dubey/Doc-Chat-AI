11dd9087d14866d6744074d5d104fd82d19c4564
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-03-27 16:22:52 +0530

    added separators and len function

--- backend/app/ingestion/splitter.py
+++ backend/app/ingestion/splitter.py
@@ -1,6 +1,9 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def split_documents(documents):
-    splitter = RecursiveCharacterTextSplitter ( chunk_size = 500, chunk_overlap = 100)
+    splitter = RecursiveCharacterTextSplitter ( chunk_size = 500, 
+                                               chunk_overlap = 100, 
+                                               length_function = len, 
+                                               separators=["\n\n", "\n", " ", ""])
     chunks = splitter.split_documents(documents)
     return chunks
