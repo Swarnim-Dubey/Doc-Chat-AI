2138f03c08d88e0255292e7d7aebae506ab60993
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-03-27 19:01:28 +0530

    testing embeddings

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from backend.app.ingestion.loader import load_document
 from backend.app.ingestion.splitter import split_documents
+from backend.app.embeddings.embedder import get_embedding_model
 
-docs = load_document("D:\\Code\\02_Project\Doc-Chat-AI\\backend\\app\\ingestion\\dsa-viva.pdf")
+docs = load_document("file.txt")
 chunks = split_documents(docs)
 
-print("Chunks : ", len(chunks))
-print(chunks[0].page_content[:200])
+embedding_model = get_embedding_model()
+
+vector = embedding_model.embed_query(chunks[0].page_content)
+
+print("Vector length:", len(vector))
+print(vector[:5])
+
+# print("Chunks : ", len(chunks))
+# print(chunks[0].page_content[:200])
+# print("first chunk : ", chunks[0].page_content[:200])
+# print("second chunk : ", chunks[1].page_content[:200])
 # print(len(docs))
 # print(docs[0].page_content[:200])
