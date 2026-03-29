101f904cc7a951cbe23dfaf7f9dfc4bb0ac8f990
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-03-29 22:54:59 +0530

    testing only

--- backend/vector_db/chroma.py
+++ backend/vector_db/chroma.py
@@ -1,8 +1,9 @@
 import chromadb
-
+from backend.app.embeddings.embedder import embeddings
 client = chromadb.PersistentClient(path="D:\\Code\\02_Project\\Doc-Chat-AI\\backend\\vector_db")
 
 collection = client.get_or_create_collection(
     name="title_embeddings",
+    embedding_function=embeddings,
     metadata={"hnsw:space": "cosine"}
 )
