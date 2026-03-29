5ad9e98fd7516f4b69db0c6d4ba9dcc9842e96ad
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-03-29 22:55:43 +0530

    testing

--- backend/app/embeddings/embedder.py
+++ backend/app/embeddings/embedder.py
@@ -23,3 +23,27 @@ if __name__ == "__main__":
     
     print("Vector length:", len(vector))
     print(vector[:5])
+
+# from langchain_google_genai import GoogleGenerativeAIEmbeddings
+# from dotenv import load_dotenv
+# import os
+
+# # Load env variables
+# load_dotenv()
+
+# # Initialize embeddings
+# embeddings = GoogleGenerativeAIEmbeddings(
+#     model="models/text-embedding-004",
+#     google_api_key=os.getenv("GOOGLE_API_KEY")
+# )
+
+# def embed_text(text: str):
+#     return embeddings.embed_query(text)
+
+
+# if __name__ == "__main__":
+#     sample = "Hello from Doc-Chat-AI"
+#     vector = embed_text(sample)
+
+#     print("Vector length:", len(vector))
+#     print(vector[:5])
