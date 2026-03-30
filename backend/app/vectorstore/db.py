c273b8569ec311d6c31e8a21774c93269d718309
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-03-30 23:01:29 +0530

    vector database testing

---
+++ backend/app/vectorstore/db.py
@@ -0,0 +1,11 @@
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
+
+def get_vectorstore():
+    embedding = HuggingFaceEmbeddings(
+        model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    )
+    return Chroma(
+        persist_directory = "vector_db",
+        embedding_function = embedding
+    )
