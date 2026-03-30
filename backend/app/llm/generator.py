0e415e3a0680ce77c44684588f6b41ddbe5dc301
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-03-30 23:00:34 +0530

    generator

--- backend/app/llm/generator.py
+++ backend/app/llm/generator.py
@@ -1,28 +1,23 @@
 import requests
 import os
 
-OLLAMA_URL = os.getenv("OLLAMA_URL", "http:localhost:11434")
+OLLAMA_URL = os.getenv("OLLAMA_URL", "http://localhost:11434")
 
 def generate_response(query, context):
     prompt = f"""
-You are a helpful AI assistant.
-
 Answer the question using ONLY the context below.
-If the answer is not in the context, say "I don't know".
 
 Context:
 {context}
 
 Question:
 {query}
-
-Answer:
 """
 
     response = requests.post(
         f"{OLLAMA_URL}/api/generate",
         json={
-            "model": "mistral",
+            "model": "llama3",
             "prompt": prompt,
             "stream": False
         }
