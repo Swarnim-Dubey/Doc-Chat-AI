0f5f17b5dec762b3e2f6dae314d856172b4240a5
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-03-30 23:09:12 +0530

    added the logic part

--- backend/app/retrieval/retriever.py
+++ backend/app/retrieval/retriever.py
@@ -1,1 +1,13 @@
 from app.vectorstore.db import get_vectorstore
+
+def retrive_docs(query:str, k: int=3):
+    """
+    Retrive top-k relavent documents from the vector store
+    """
+
+    db = get_vectorstore()
+
+    #similarity search
+    docs = db.similarity_search(query, k=k)
+
+    return docs
