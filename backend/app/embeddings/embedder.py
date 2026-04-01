60bad446fb89e641cfc27fce2657b8fd8bf0128d
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-04-01 21:47:23 +0530

    removed google embeddings

--- backend/app/embeddings/embedder.py
+++ backend/app/embeddings/embedder.py
@@ -1,17 +1,7 @@
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from vertexai.language_models import TextEmbeddingModel
-from dotenv import load_dotenv
-import os
-import vertexai
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 
-#loading env variables here
-load_dotenv()
-
-vertexai.init(project="")
-# initializing gemini embeddings
-embeddings = GoogleGenerativeAIEmbeddings(
-    model="models/text-embedding-004",
-    google_api_key = os.getenv("GOOGLE_API_KEY") # google gemini api key
+embeddings = HuggingFaceBgeEmbeddings(
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
 )
 
 def embed_text(text : str):
@@ -20,30 +10,6 @@ def embed_text(text : str):
 if __name__ == "__main__":
     sample = "Hello from Doc-Chat-AI"
     vector = embed_text(sample)
-    
-    print("Vector length:", len(vector))
+
+    print("Vector: ", len(vector))
     print(vector[:5])
-
-# from langchain_google_genai import GoogleGenerativeAIEmbeddings
-# from dotenv import load_dotenv
-# import os
-
-# # Load env variables
-# load_dotenv()
-
-# # Initialize embeddings
-# embeddings = GoogleGenerativeAIEmbeddings(
-#     model="models/text-embedding-004",
-#     google_api_key=os.getenv("GOOGLE_API_KEY")
-# )
-
-# def embed_text(text: str):
-#     return embeddings.embed_query(text)
-
-
-# if __name__ == "__main__":
-#     sample = "Hello from Doc-Chat-AI"
-#     vector = embed_text(sample)
-
-#     print("Vector length:", len(vector))
-#     print(vector[:5])
