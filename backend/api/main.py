ea8e2bf1dfad22c2801717f9f3d9b42ad4a556bb
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-04-01 21:46:26 +0530

    refractured code

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,5 +1,13 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from api.routes import router
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # for dev only
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
