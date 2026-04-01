184599ddb5859f18fd6b7385f9449e49d23f5e06
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-04-01 21:48:20 +0530

    made it correct

--- backend/app/vectorstore/db.py
+++ backend/app/vectorstore/db.py
@@ -1,11 +1,11 @@
 from langchain_community.vectorstores import Chroma
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 def get_vectorstore():
     embedding = HuggingFaceEmbeddings(
         model_name = "sentence-transformers/all-MiniLM-L6-v2"
     )
     return Chroma(
-        persist_directory = "vector_db",
+        persist_directory = "backend/vector_db",
         embedding_function = embedding
     )
