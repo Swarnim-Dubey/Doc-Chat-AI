6cb43589a82da674c5e15048ba229ab2a655848a
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-04-05 17:28:46 +0530

    refactored

--- backend/app/retrieval/retriever.py
+++ backend/app/retrieval/retriever.py
@@ -1,6 +1,6 @@
 from app.vectorstore.db import get_vectorstore
 
-def retrive_docs(query:str, k: int=3):
+def retrieve_docs(query:str, k: int=3):
     """
     Retrive top-k relavent documents from the vector store
     """
@@ -10,4 +10,4 @@ def retrive_docs(query:str, k: int=3):
     #similarity search
     docs = db.similarity_search(query, k=k)
 
-    return docs
+    return [doc.page_content for doc in docs]
