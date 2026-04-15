8f92f940bef1c93e77d3ea8451f9b9eac648890e
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-04-16 00:48:39 +0530

    refactored the loader behavior

--- backend/app/ingestion/loader.py
+++ backend/app/ingestion/loader.py
@@ -1,14 +1,29 @@
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from app.vectorstore.db import get_vectorstore
+import os
 
-def load_document(file_path : str):
+def load_document(file_path: str):
+    # Load file
     if file_path.lower().endswith(".pdf"):
         loader = PyPDFLoader(file_path)
-
     elif file_path.lower().endswith(".txt"):
         loader = TextLoader(file_path)
-
     else:
         raise ValueError("Unsupported filetype")
-    
+
     documents = loader.load()
-    return documents
+
+    # Split into chunks
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=50
+    )
+    chunks = splitter.split_documents(documents)
+
+    # Store in vector DB
+    db = get_vectorstore()
+    db.add_documents(chunks)
+    db.persist()
+
+    return len(chunks)
