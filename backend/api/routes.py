8cabdd6d3b8b49d9c1f94df90c3eaf2de60af0d6
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-04-15 23:50:06 +0530

    added the /upload route in api

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,11 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File
 from pydantic import BaseModel
 from app.pipeline import run_pipeline
+from app.ingestion.loader import load_document
 
 router = APIRouter()
 
 class QueryRequest(BaseModel):
     query : str
+
+@router.post("/upload")
+async def upload(file: UploadFile = File(...)):
+    content = await file.read()
+    load_document(file.filename, content)
+    return{"message" : "Document Uploadad Successfully"}
 
 @router.post("/chat")
 def chat(q: QueryRequest):
