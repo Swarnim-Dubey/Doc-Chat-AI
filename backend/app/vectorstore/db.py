5cac0e7bba83594c3254c884362ad861045d34ec
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-04-16 21:17:03 +0530

    fixed a bug

--- backend/app/vectorstore/db.py
+++ backend/app/vectorstore/db.py
@@ -1,11 +1,16 @@
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
+import os
 
-def get_vectorstore():
+def get_vectorstore(file_name: str):
     embedding = HuggingFaceEmbeddings(
-        model_name = "sentence-transformers/all-MiniLM-L6-v2"
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
+    persist_dir = f"backend/vector_db/{file_name}"
+
+    os.makedirs(persist_dir, exist_ok=True)
+
     return Chroma(
-        persist_directory = "backend/vector_db",
-        embedding_function = embedding
+        persist_directory=persist_dir,
+        embedding_function=embedding
     )
