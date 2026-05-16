90ed6c1d0a00340b3de25954ba904d1b103d35ee
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-05-16 23:10:43 +0530

    refactor

--- backend/app/ingestion/loader.py
+++ backend/app/ingestion/loader.py
@@ -1,29 +1,40 @@
-from langchain_community.document_loaders import PyPDFLoader, TextLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from app.vectorstore.db import get_vectorstore
 import os
 
+from langchain_community.document_loaders import PyPDFLoader,TextLoader
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from backend.app.vectorstore.db import get_vectorstore
+
+
 def load_document(file_path: str):
-    # Load file
     if file_path.lower().endswith(".pdf"):
         loader = PyPDFLoader(file_path)
     elif file_path.lower().endswith(".txt"):
         loader = TextLoader(file_path)
     else:
-        raise ValueError("Unsupported filetype")
+        raise ValueError("Unsupported file type")
 
     documents = loader.load()
 
-    # Split into chunks
+    print("\nLoaded pages:", len(documents))
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=500,
         chunk_overlap=50
     )
     chunks = splitter.split_documents(documents)
 
-    # Store in vector DB
-    db = get_vectorstore()
-    db.add_documents(chunks)
-    db.persist()
+    print("Chunks created:", len(chunks))
 
-    return len(chunks)
+    if len(chunks) > 0:
+        print("\nFIRST CHUNK:\n")
+        print(chunks[0].page_content[:500])
+    file_name = os.path.basename(file_path)
+
+    db = get_vectorstore(file_name)
+    db.add_documents(chunks)
+    # db.persist()
+
+    print("\nStored in vector DB")
+    print("Total vectors:", db._collection.count())
+
+    return chunks
