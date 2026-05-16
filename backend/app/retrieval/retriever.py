fdb4c817399dbf8732d3d49f2a83c7855a5f448b
Swarnim-Dubey <swarnimdubey135@gmail.com>
2026-05-16 23:11:03 +0530

    refactor

--- backend/app/retrieval/retriever.py
+++ backend/app/retrieval/retriever.py
@@ -2,7 +2,9 @@ from app.vectorstore.db import get_vectorstore
 
 def retrieve_docs(query: str, file_name: str, k: int = 3):
     db = get_vectorstore(file_name)
+    print("\nSearching in: ", file_name)
+    print("vectors available: ", db._collection.count())
 
     docs = db.similarity_search(query, k=k)
-
+    print("Retrieved documents:", len(docs))
     return docs
